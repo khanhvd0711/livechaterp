ea55bbaa1721348822f5e485ce19d038d4fa0f03
khanhvd0711 <livechat123@>
2018-10-04 09:55:30 +0700

    commit

---
+++ src/components/MessageBox.js
@@ -0,0 +1,53 @@
+import React, {Component} from 'react';
+
+class MessageBox extends Component{
+
+    constructor(props){
+        super(props);
+        this.onChange = this.onChange.bind(this);
+        this.onKeyup = this.onKeyup.bind(this);
+        this.state = {
+            chatlist: '',
+        };
+
+        //this.setState({});}
+    }
+
+    onChange(e){
+        this.setState({
+            chatlist: e.target.value,
+        });
+    }
+    onKeyup(e){
+        if(e.keyCode === 13 && (e.target.value).trim() !== ''){
+            e.preventDefault();
+            var ip = require("ip");
+            console.log(ip.address());
+            let dbCon = this.props.db.database().ref('/chatlist');
+            dbCon.push({
+                message: (e.target.value).trim(),
+                time: new  Date().toLocaleString(),
+                ipAddress: ip.address()
+            });
+            this.setState({
+                chatlist: '',
+            });
+        }
+    }
+    render(){
+        return(
+            <form>
+            <textarea
+                className="textarea"
+                placeholder="Type a message"
+                cols="100"
+                onChange={this.onChange}
+                onKeyUp={this.onKeyup}
+                value={this.state.chatlist}>
+              </textarea>
+            </form>
+        )
+    }
+}
+
+export default MessageBox
---
+++ src/components/MessageList.js
@@ -0,0 +1,53 @@
+import React, {Component} from 'react';
+import Message from './Message';
+import _ from 'lodash';
+
+class MessageList extends Component {
+    constructor(props){
+        super(props);
+        this.state  = {
+            chatlist: []
+        };
+        let app =this.props.db.database().ref('chatlist');
+
+        app.on('value', snapshot => {
+            this.getData(snapshot.val());
+        });
+    }
+
+    getData(values){
+        let messagesVal = values;
+        let chatlist = _(messagesVal)
+            .keys()
+            .map(messageKey => {
+                let cloned = _.clone(messagesVal[messageKey]);
+                cloned.key = messageKey;
+                return cloned;
+            })
+            .value();
+        this.setState({
+            chatlist: chatlist
+        });
+    }
+
+
+    render(){
+        let messageNodes = this.state.chatlist.map((message, index) => {
+            return (
+                <div key={ index } className="card">
+                    <div className="card-content">
+                        <i> <Message message = { message.time} /> : </i>
+                        <Message message = {message.message} />
+                    </div>
+                </div>
+            )
+        });
+        return (
+            <div>
+                {messageNodes}
+            </div>
+        );
+    }
+}
+
+export default MessageList
